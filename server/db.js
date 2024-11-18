39e69277e355ce87fca48b009c26433821784ef5
Dimitar <dimitar_dimitrov12@mail.bg>
2024-11-18 14:47:29 +0200

    add new DB and port add script for create table
    add new DB and port add script for create table

--- server/db.js
+++ server/db.js
@@ -3,9 +3,9 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "mitaka",
   password: "mitaka",
-  host: "localhost",
+  host: "192.168.55.5",
   // port: 6543,// home
-  port: 5432,// work
+  port: 5434,// work
   database: "postgres"
 });
 
