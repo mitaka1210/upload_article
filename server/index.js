8fe8cc26ec962ca814e7d1c4a6c7d503bff60fe2
Dimitar <dimitar_dimitrov12@mail.bg>
2025-01-22 14:43:23 +0200

    create-module-request
    create-module-request refactor code create component for all api

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import pool from "./db.js";
+import pool from "./config/db.js";
 
 import multer from "multer";
 import path from "path";
--- server/test.js
+++ server/test.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db");
+const pool = require("./config/db.js");
 const multer = require("multer");
 
 /**
