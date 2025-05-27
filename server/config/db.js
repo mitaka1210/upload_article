337eb5cc07579163ae4f65d715f371faca13b3fc
Dimitar <dimitar_dimitrov12@mail.bg>
2025-05-27 16:46:41 +0300

    feature/MB-6 add 2 place for development environment prod and dev
    feature/MB-6 add 2 place for development environment prod and dev

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,26 @@
 import pkg from "pg";
+import dotenv from "dotenv";
 
 const { Pool } = pkg;
-
-const pool = new Pool({
-  user: "mitaka",
-  password: "mitaka",
-  host: "192.168.55.5",
-  port: 5434, // work
-  database: "postgres",
+// Зареждане на подходящия .env файл
+dotenv.config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? ".env.production"
+      : ".env.development",
 });
-
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+// const pool = new Pool({
+//   user: "mitaka",
+//   password: "mitaka",
+//   host: "192.168.55.5",
+//   port: "5434",
+//   database: "postgres",
+// });
 export default pool;
